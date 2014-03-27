fc6e1cb0277f0ccb3030992636cadc5158982a4d
mclo <mclo0202@gmail.com>
2014-03-26 22:20:06 -0300

    update App class

--- src/main/java/ar/edu/unlp/CellularAutomaton/App.java
+++ src/main/java/ar/edu/unlp/CellularAutomaton/App.java
@@ -11,18 +11,26 @@ public class App
 {
 	/**
 	 * Launch the application.
+	 * @param args args[0] colums, args[1] rows, args[2] generations
 	 */
 	public static void main(String[] args) {
 		
 		int cols = Integer.valueOf(args[0]);
 		int rows = Integer.valueOf(args[1]);
 		int generations  = Integer.valueOf(args[2]);
+		long time_start, time_end;
+		
+		time_start = System.currentTimeMillis();
 		
 		GameOfLifeGrid grid = new GameOfLifeGrid(cols, rows);
 		
 		for (int i = 0; i < generations; i++) {
 			grid.nextGeneration();
-			System.out.println(grid.getGeneration());
 		}
+		
+		time_end = System.currentTimeMillis();
+		System.out.println(grid);
+		System.out.println("generations: "+grid.getGeneration());
+		System.out.println("The task has taken "+ ( time_end - time_start ) +" milliseconds");
 	}
 }
